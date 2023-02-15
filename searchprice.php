b5b343e3561f486d34ff4410c1ecef28c11fc5b4
sphy1597 <sphy1597@naver.com>
2023-02-15 13:41:54 +0900

    Update searchprice.php

--- searchprice.php
+++ searchprice.php
@@ -1,6 +1,7 @@
-url = 'https://api.neople.co.kr/df/auction?itemName=%ec%a7%84+%eb%af%b8%ec%8a%a4%ed%8a%b8%eb%a6%ac%ec%8a%a4%ec%9d%98+%eb%8b%a8%eb%b0%9c+%ec%9b%a8%ec%9d%b4%eb%b8%8c%5bc%ed%83%80%ec%9e%85%5d&wordType=front&apikey=c5mNPM0xWNNvg9qBQv4HyBZQOXSfbfLb'
+<?php
+    include "simple_html_dom.php";
 
-$html = file_get_html (url);
+    $html = file_get_html ('https://api.neople.co.kr/df/auction?itemName=%ec%a7%84+%eb%af%b8%ec%8a%a4%ed%8a%b8%eb%a6%ac%ec%8a%a4%ec%9d%98+%eb%8b%a8%eb%b0%9c+%ec%9b%a8%ec%9d%b4%eb%b8%8c%5bc%ed%83%80%ec%9e%85%5d&wordType=front&apikey=c5mNPM0xWNNvg9qBQv4HyBZQOXSfbfLb');
 
-echo $ret = $html->find( 'pre' );
-
+    echo $ret = $html->find( 'pre' );
+?>
