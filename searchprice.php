4b7630404479ed50d7f3533bb6f324d3b5d44ab0
sphy1597 <101171867+sphy1597@users.noreply.github.com>
2023-02-15 13:09:13 +0900

    Create searchprice.php

---
+++ searchprice.php
@@ -0,0 +1,6 @@
+url = 'https://api.neople.co.kr/df/auction?itemName=%ec%a7%84+%eb%af%b8%ec%8a%a4%ed%8a%b8%eb%a6%ac%ec%8a%a4%ec%9d%98+%eb%8b%a8%eb%b0%9c+%ec%9b%a8%ec%9d%b4%eb%b8%8c%5bc%ed%83%80%ec%9e%85%5d&wordType=front&apikey=c5mNPM0xWNNvg9qBQv4HyBZQOXSfbfLb'
+
+$html = file_get_html (url);
+
+echo $ret = $html->find( 'pre' );
+
